c17f21001ee9ebbf082f0f377461ca9fa1e20083
Aditya Ramdas Bangar <231313460+AdityaBangar27@users.noreply.github.com>
2026-08-08 15:25:59 +0530

    Time: 1 ms (49.39%), Space: 46.7 MB (70.48%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        int index = 0;
+
+        // insert even numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+
+        // insert odd numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
