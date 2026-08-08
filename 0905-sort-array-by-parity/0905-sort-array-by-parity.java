c9ab306733717e2933e61be43eaaef8f35face9c
Aditya Ramdas Bangar <231313460+AdityaBangar27@users.noreply.github.com>
2026-08-08 15:35:44 +0530

    Time: 0 ms (100%), Space: 46.7 MB (70.48%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,26 +1,42 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
 
-        int[] result = new int[nums.length];
+        // int[] result = new int[nums.length];
 
-        int index = 0;
+        // int index = 0;
 
-        // insert even numbers
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                result[index] = nums[i];
-                index++;
+        // // insert even numbers
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] % 2 == 0) {
+        //         result[index] = nums[i];
+        //         index++;
+        //     }
+        // }
+
+        // // insert odd numbers
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] % 2 != 0) {
+        //         result[index] = nums[i];
+        //         index++;
+        //     }
+        // }
+
+        int i = 0;
+        int j = nums.length-1;
+
+        while(i < j){
+            if(nums[i] % 2 == 0){
+                i++;
+            }else if(nums[j] % 2 != 0){
+                j--;
+            }else{
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
             }
         }
-
-        // insert odd numbers
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 != 0) {
-                result[index] = nums[i];
-                index++;
-            }
-        }
-
-        return result;
+        return nums;
     }
 }
