61e90e41035f87e321b82a774150a886a090e289
Aditya Ramdas Bangar <231313460+AdityaBangar27@users.noreply.github.com>
2026-08-22 23:21:31 +0530

    Time: 42 ms (60.77%), Space: 53.2 MB (81.35%) - LeetHub

--- 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -1,10 +1,12 @@
-var fibGenerator = function*() {
+var fibGenerator = function* () {
     let a = 0;
     let b = 1;
 
     while (true) {
         yield a;
-        [a, b] = [b, a + b];
+        let temp = a;
+        a = b;
+        b = temp + b;
     }
 };
 
@@ -13,3 +15,18 @@ var fibGenerator = function*() {
  * gen.next().value; // 0
  * gen.next().value; // 1
  */
+
+//  class Solution {
+//     public int[] generateFibonacci(int n) {
+//         int[] ans = new int[n];
+
+//         if (n > 0) ans[0] = 0;
+//         if (n > 1) ans[1] = 1;
+
+//         for (int i = 2; i < n; i++) {
+//             ans[i] = ans[i - 1] + ans[i - 2];
+//         }
+
+//         return ans;
+//     }
+// }
