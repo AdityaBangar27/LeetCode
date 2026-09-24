358de6e5c3446d41870f3c6ea005a149085b71f1
Aditya Ramdas Bangar <231313460+AdityaBangar27@users.noreply.github.com>
2026-09-24 22:39:59 +0530

    Time: 0 ms (100%), Space: 46.8 MB (7.44%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,36 +1,43 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
 
-    //    int ans[] = new int[n*2];
-    //    int j = 0;
+       int ans[] = new int[n*2];
+       int j = 0;
 
-    //    for(int i = 0; i < n; i++){
-    //     ans[j++] = nums[i];
-    //     ans[j++] = nums[i+n];
-    //    }
+       for(int i = 0; i < n; i++){
+        ans[j++] = nums[i];
+        ans[j++] = nums[i+n];
+       }
+       return ans;
+
+
+
+    //     Queue<Integer> q = new LinkedList<>();
+    //     for(int ele : nums){
+    //         q.add(ele);
+    //     }
+    //     int size = q.size();
+    //     Queue<Integer> half = new LinkedList<>();
+    //     for(int i = 0; i < size/2; i++){
+    //         half.add(q.remove());
+    //     }
+
+    //     while(!half.isEmpty()){
+    //         q.add(half.remove());
+    //         q.add(q.remove());
+    //     }
+    //     int[] ans = new int[q.size()];
+
+    //     int i = 0;
+    //     for (int ele : q) {
+    //         ans[i++] = ele;
+    //     }
+
     //    return ans;
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int ele : nums){
-            q.add(ele);
-        }
-        int size = q.size();
-        Queue<Integer> half = new LinkedList<>();
-        for(int i = 0; i < size/2; i++){
-            half.add(q.remove());
-        }
-
-        while(!half.isEmpty()){
-            q.add(half.remove());
-            q.add(q.remove());
-        }
-        int[] ans = new int[q.size()];
-
-        int i = 0;
-        for (int ele : q) {
-            ans[i++] = ele;
-        }
-
-       return ans;
+        // int ans[] = new int[2*n];
+        // for(int i = 0; i < n; i++){
+        //     ans[i]
+        // }
     }
 }
