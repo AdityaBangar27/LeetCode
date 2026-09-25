0721f43490f43bc94f45da2d3617fda870a6b12d
Aditya Ramdas Bangar <231313460+AdityaBangar27@users.noreply.github.com>
2026-09-25 23:32:03 +0530

    Time: 3 ms (7%), Space: 94.6 MB (33.1%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -12,19 +12,29 @@ class Solution {
         // return maxProfit;.
 
         //Two pointers 
-        int buy = 0;
-        int sell = 1;
-        int maxProfit = 0;
+        // int buy = 0;
+        // int sell = 1;
+        // int maxProfit = 0;
 
-        while(sell < prices.length){
-            if(prices[buy] < prices[sell]){
-                int profit = prices[sell] - prices[buy];
-                maxProfit = Math.max(profit,maxProfit);
-            }else{
-                buy = sell;
-            }
-            sell++;
+        // while(sell < prices.length){
+        //     if(prices[buy] < prices[sell]){
+        //         int profit = prices[sell] - prices[buy];
+        //         maxProfit = Math.max(profit,maxProfit);
+        //     }else{
+        //         buy = sell;
+        //     }
+        //     sell++;
+        // }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i++){
+            minPrice = Math.min(minPrice,prices[i]);
+            int profit = prices[i] - minPrice;
+
+            maxProfit = Math.max(profit,maxProfit);
         }
+
         return maxProfit;
     }
 }
